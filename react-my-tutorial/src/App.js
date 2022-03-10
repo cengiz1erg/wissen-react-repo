620bb8dc6038481265f5cb172afcd2fc15c47576
Cengiz Ergün <cengiz1ergun@gmail.com>
2022-03-10 15:49:17 +0300

    Routing

--- react-my-tutorial/src/App.js
+++ react-my-tutorial/src/App.js
@@ -1,9 +1,20 @@
-import SupplierAdd from "./fetchSample/SupplierAdd";
+import { Route, Routes } from "react-router-dom";
+import AboutPage from "./routingSample/AboutPage";
+import ContactPage from "./routingSample/ContactPage";
+import Home from "./routingSample/Home";
+import SupplierDetail from "./routingSample/SupplierDetail";
+import SupplierPage from "./routingSample/SupplierPage";
 
 function App() {
   return (
     <>
-        <SupplierAdd></SupplierAdd>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<AboutPage/>}></Route>
+        <Route path='/contact' element={<ContactPage/>}></Route>
+        <Route path='/suppliers' element={<SupplierPage/>}></Route>
+        <Route path='/suppliers/detail/:id' element={<SupplierDetail/>}></Route>
+      </Routes>
     </>
   );
 }
